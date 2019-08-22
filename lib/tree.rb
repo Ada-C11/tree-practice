58fbe7801a90829899f4e6026af608454351392f
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-08-22 16:29:09 -0700

    wrote add and find methods to make tests pass

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,59 +2,81 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
     @value = val
     @left = nil
     @right = nil
-   end
+  end
 end
 
 class Tree
   attr_reader :root
+
   def initialize
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def add(key, value)
-    raise NotImplementedError
+    new_tree_node = TreeNode.new(key, value)
+
+    # if @root.nil?
+      @root = new_tree_node
+    # else
+    #   current = @root
+
+    #   while !current.left.value.nil? || !current.right.value.nil?
+
+    #   end
+    # end 
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def find(key)
-    raise NotImplementedError
+    return nil if @root.nil?
+
+    current = @root
+    until current.key == key
+      if key > current.key
+        current = current.right
+      else 
+        current = current.left
+      end 
+    end 
+
+    return current.value
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def inorder
     raise NotImplementedError
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def preorder
     raise NotImplementedError
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def postorder
     raise NotImplementedError
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def height
     raise NotImplementedError
   end
 
   # Optional Method
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def bfs
     raise NotImplementedError
   end
