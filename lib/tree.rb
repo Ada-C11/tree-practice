208f8166ab0db7fd6f84132322aec48d35553e1d
amyesh <amyesh08@gmail.com>
2019-09-01 13:06:46 -0700

    completed add find in pre post methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,61 +2,123 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
     @value = val
     @left = nil
     @right = nil
-   end
+  end
+
+  def add(key, val)
+    if key <= @key
+      if !left
+        @left = TreeNode.new(key, val)
+      else
+        @left.add(key, val)
+      end
+    else
+      if !right
+        @right = TreeNode.new(key, val)
+      else
+        @right.add(key, val)
+      end
+    end
+  end
+
+  def find(key)
+    if key == @key
+      return @value
+    elsif key < @key
+      return nil if !@left
+      return @left.find(key)
+    else
+      return nil if !@right
+      return @right.find(key)
+    end
+  end
+
+  def inorder(tree)
+    left.inorder(tree) unless !left
+    tree << {key: @key, value: @value}
+    right.inorder(tree) unless !right
+  end
+
+  def preorder(tree)
+    tree << {key: @key, value: @value}
+    left.preorder(tree) unless !left
+    right.preorder(tree) unless !right
+  end
+
+  def postorder(tree)
+    left.postorder(tree) unless !left
+    right.postorder(tree) unless !right
+    tree << {key: @key, value: @value}
+  end
 end
 
 class Tree
   attr_reader :root
+
   def initialize
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(1)
   def add(key, value)
-    raise NotImplementedError
+    if !@root
+      @root = TreeNode.new(key, value)
+    else
+      @root.add(key, value)
+    end
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(1)
   def find(key)
-    raise NotImplementedError
+    return nil if !@root
+    return @root.find(key)
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def inorder
-    raise NotImplementedError
+    tree = []
+    return tree if !@root
+    @root.inorder(tree)
+    return tree
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity:O(n)
   def preorder
-    raise NotImplementedError
+    tree = []
+    return tree if !@root
+    @root.preorder(tree)
+    return tree
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def postorder
-    raise NotImplementedError
+    tree = []
+    return tree if !@root
+    @root.postorder(tree)
+    return tree
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def height
-    raise NotImplementedError
+
+    #everytime you go to a branch, add 1 to count that starts at 1
+    # do both sides, if one is greater return that one
   end
 
   # Optional Method
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity:
+  # Space Complexity:
   def bfs
-    raise NotImplementedError
   end
 
   # Useful for printing
