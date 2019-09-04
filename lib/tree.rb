ee8e4fd4ca322ed70b6b6c06f3e58fb83bab7abf
qqdipps <sdipp007@gmail.com>
2019-09-04 09:29:06 -0700

    implemented methods and added unit tests for hieght

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,37 +19,103 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+    if !@root 
+      @root = new_node
+      return 
+    end
+    add_node(new_node, @root)
+
   end
+  
+  def add_node(new_node, curr)
+    if curr.key >= new_node.key
+      if !curr.left 
+        curr.left = new_node
+        return
+      end
+      add_node(new_node, curr.left)
+    else
+      if !curr.right
+        curr.right = new_node
+        return
+      end
+      add_node(new_node, curr.right)
+    end
+  end
+
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return if !@root
+    curr = @root
+    while curr 
+      if curr.key == key
+        return curr.value
+      elsif curr.key > key
+        curr = curr.left
+      else
+        curr = curr.right
+      end
+    end
+    return false
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    inorder_traversal(@root, [])
+  end
+
+  def inorder_traversal(node, node_array)
+    return node_array if !node
+    inorder_traversal(node.left, node_array)
+    visit(node, node_array)
+    inorder_traversal(node.right, node_array)
+  end
+
+  def visit(node, arr)
+    arr.push({key: node.key, value: node.value})
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    preorder_traversal(@root, [])
+  end
+
+  def preorder_traversal(node, node_array)
+    return node_array if !node
+    visit(node, node_array)
+    preorder_traversal(node.left, node_array)
+    preorder_traversal(node.right, node_array)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    postorder_traversal(@root, [])
+  end
+
+  def postorder_traversal(node, node_array)
+    return node_array if !node
+    postorder_traversal(node.left, node_array)
+    postorder_traversal(node.right, node_array)
+    visit(node, node_array)
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def height
-    raise NotImplementedError
+    longest_tree_path(@root)
+  end
+
+  def longest_tree_path(node)
+    return 0 if !node
+    right = longest_tree_path(node.right)
+    left = longest_tree_path(node.left)
+    return [right, left].max + 1
   end
 
   # Optional Method
--- test/tree_test.rb
+++ test/tree_test.rb
@@ -18,13 +18,12 @@ describe Tree do
 
   it "add & find values" do
     tree.add(5, "Peter")
-    expect(tree.find(5)).must_equal "Peter"
-
-    tree.add(15, "Ada")
-    expect(tree.find(15)).must_equal "Ada"
-
     tree.add(3, "Paul")
+    tree.add(15, "Ada")
+    expect(tree.find(5)).must_equal "Peter"
     expect(tree.find(3)).must_equal "Paul"
+    expect(tree.find(15)).must_equal "Ada"
+    
   end
 
   it "can't find anything when the tree is empty" do
@@ -69,15 +68,30 @@ describe Tree do
     end
   end
 
-  describe "breadth first search" do
-    it "will give an empty array for an empty tree" do
-      expect(tree.bfs).must_equal []
+  describe "hieght" do 
+    it "will return 0 if tree is empty" do 
+      expect(tree.height()).must_equal 0
     end
 
-    it "will return an array of a level-by-level output of the tree" do
-      expect(tree_with_nodes.bfs).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"}, 
-                                   {:key=>10, :value=>"Karla"}, {:key=>1, :value=>"Mary"}, 
-                                   {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
+    it "will return the nuber of nodes in the longest path" do
+      expect(tree_with_nodes.height).must_equal 4
+      tree_with_nodes.add(60, "sam")
+      tree_with_nodes.add(58, "penny")
+      tree_with_nodes.add(65, "sam")
+      expect(tree_with_nodes.height).must_equal 6
     end
+
   end
+
+  # describe "breadth first search" do
+  #   it "will give an empty array for an empty tree" do
+  #     expect(tree.bfs).must_equal []
+  #   end
+
+  #   it "will return an array of a level-by-level output of the tree" do
+  #     expect(tree_with_nodes.bfs).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"}, 
+  #                                  {:key=>10, :value=>"Karla"}, {:key=>1, :value=>"Mary"}, 
+  #                                  {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
+  #   end
+  # end
 end
