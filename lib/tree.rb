9764fa130a8d34e4808862d58ea117fb83cc3b79
Jillianne Ramirez <jillianne.ramirez@alaskaair.com>
2019-09-12 12:43:15 -0700

    solutions to tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,31 +19,94 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    new_node = TreeNode.new(key, value)
+    if @root == nil
+      @root = new_node
+    else
+      node = @root
+      add_value(node, new_node)
+    end
+  end
+
+  def add_value(current, new_node)
+    if !current
+      current = new_node
+    elsif current.key < new_node.key
+      add_value(current.right, new_node)
+    else
+      add_value(current.left, new_node)
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return nil if @head == nil
+    find_value(@head, key)
+  end
+
+  def find_value(current, key)
+    if current.key == key
+      return current.value
+    elsif current.key > key
+      find_value(current.right, key)
+    else
+      find_value(current.left, key)
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return [] if @head == nil
+    return inorder_key(@head)
+  end
+
+  def inorder_key(current_node)
+    values = []
+    if !current_node
+      return values
+    else
+      inorder_key(current_node.left)
+      values << {key: current.key, value: current.value}
+      inorder_key(current_node.right)
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def preorder
-    raise NotImplementedError
+    return [] if @head == nil
+    return preorder_key(@head)
+  end
+
+  def preorder_key(current_node)
+    values = []
+    if !current_node
+      return values
+    else
+      values << {key: current.key, value: current.value}
+      preorder_key(current_node.left)
+      preorder_key(current_node.right)
+    end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def postorder
-    raise NotImplementedError
+    return [] if @head == nil
+    return postorder_key(@head)
+  end
+
+  def postorder_key(current_node)
+    values = []
+    if !current_node
+      return values
+    else
+      postorder_key(current_node.left)
+      postorder_key(current_node.right)
+      values << {key: current.key, value: current.value}
+    end
   end
 
   # Time Complexity: 
