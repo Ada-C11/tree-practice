5927dc689aa4d2629b4ad360ea931106a92a7de5
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-09-19 15:09:40 -0700

    add and find value in binary search tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -20,57 +20,66 @@ class Tree
   # Space Complexity: 
   def add(key, value)
     new_node = TreeNode.new(key, value)
-    if @root == nil
+    if !@root
       @root = new_node
     else
-      node = @root
-      add_value(node, new_node)
+      add_value(@root, new_node)
     end
   end
 
+  # TODO: Refactor solution with loop
   def add_value(current, new_node)
-    if !current
-      current = new_node
-    elsif current.key < new_node.key
-      add_value(current.right, new_node)
+    if current.key < new_node.key
+      if current.right
+        add_value(current.right, new_node)
+      else
+        current.right = new_node
+      end
     else
-      add_value(current.left, new_node)
+      if current.left
+        add_value(current.left, new_node)
+      else
+        current.left = new_node
+      end
     end
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    return nil if @head == nil
-    find_value(@head, key)
-  end
+    return nil if !@root
 
-  def find_value(current, key)
-    if current.key == key
-      return current.value
-    elsif current.key > key
-      find_value(current.right, key)
-    else
-      find_value(current.left, key)
+    current = @root
+
+    while current
+      return current.value if current.key == key
+      
+      if current.key < key
+        current = current.right
+      else
+        current = current.left
+      end
     end
+    return nil
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    return [] if @head == nil
-    return inorder_key(@head)
+    return [] if !@head
+    values = []
+    return inorder_key(@head, values)
   end
 
-  def inorder_key(current_node)
-    values = []
+  def inorder_key(current_node, array)
     if !current_node
-      return values
+      return array
     else
       inorder_key(current_node.left)
-      values << {key: current.key, value: current.value}
+      varray << {key: current_node.key, value: current_node.value}
       inorder_key(current_node.right)
     end
+    return
   end
 
   # Time Complexity: 
@@ -95,7 +104,7 @@ class Tree
   # Space Complexity: 
   def postorder
     return [] if @head == nil
-    return postorder_key(@head)
+    postorder_key(@head)
   end
 
   def postorder_key(current_node)
@@ -105,8 +114,9 @@ class Tree
     else
       postorder_key(current_node.left)
       postorder_key(current_node.right)
-      values << {key: current.key, value: current.value}
+      values << {key: current_node.key, value: current_node.value}
     end
+    return values
   end
 
   # Time Complexity: 
